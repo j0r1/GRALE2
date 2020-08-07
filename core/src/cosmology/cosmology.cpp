65b092757a05a1475b8605e3f19493a7441a24af
Jori Liesenborgs <jori.liesenborgs@gmail.com>
2020-08-07 18:00:44 +0200

    Spaces/tab issue, vscode detected it wrong
    Apparently vscode guesses the use of spaces/tabs, it is not linked
    to a specific file type. For new files the preference is spaces,
    and for existing files it is sometimes detected wrong (due to a
    license header?). In this, and the following commits, indentation
    has been replaced to tabs again.

--- core/src/cosmology/cosmology.cpp
+++ core/src/cosmology/cosmology.cpp
@@ -11,11 +11,11 @@ namespace grale
 
 Cosmology::Cosmology(double h, double Wm, double Wr, double Wv, double w)
 {
-    m_h = h;
-    m_Wm = Wm;
-    m_Wr = Wr;
-    m_Wv = Wv;
-    m_w = w;
+	m_h = h;
+	m_Wm = Wm;
+	m_Wr = Wr;
+	m_Wv = Wv;
+	m_w = w;
 }
 
 Cosmology::~Cosmology()
@@ -46,16 +46,16 @@ struct CosmParams
 
 double Cosmology::getAngularDiameterDistance(double z1, double z2) const
 {
-    if (z1 < 0 || z2 < 0)
-    {
-        setErrorString("Redshifts must be positive");
-        return numeric_limits<double>::quiet_NaN();
-    }
+	if (z1 < 0 || z2 < 0)
+	{
+		setErrorString("Redshifts must be positive");
+		return numeric_limits<double>::quiet_NaN();
+	}
 
-    if (z1 > z2)
-        std::swap(z1, z2);
+	if (z1 > z2)
+		std::swap(z1, z2);
 
-   	CosmParams cosm(m_h, m_Wm, m_Wr, m_Wv, m_w);
+	CosmParams cosm(m_h, m_Wm, m_Wr, m_Wv, m_w);
 	gsl_function F;
 	F.function = integrationFunction;
 	F.params = &cosm;
@@ -64,22 +64,22 @@ double Cosmology::getAngularDiameterDistance(double z1, double z2) const
 	double result = 0, abserr = 0;
 	size_t neval = 0;
 
-    int status = gsl_integration_qng(&F, 1.0/(1.0+z2), 1.0/(1.0+z1), 0, 1e-7, &result, &abserr, &neval);
+	int status = gsl_integration_qng(&F, 1.0/(1.0+z2), 1.0/(1.0+z1), 0, 1e-7, &result, &abserr, &neval);
 	if (status)
-    {
-        setErrorString("Error performing gsl_integration_qng, code " + to_string(status));
-        return numeric_limits<double>::quiet_NaN();
-    }
+	{
+		setErrorString("Error performing gsl_integration_qng, code " + to_string(status));
+		return numeric_limits<double>::quiet_NaN();
+	}
 
 	double T_H = DIST_MPC/100000.0;
 	double commonFactor = 1.0/(1.0+z2) * SPEED_C*T_H/m_h;
 
 	if (cosm.Wk == 0)
 		return commonFactor * result;
-	
+
 	if (cosm.Wk < 0)
 		return commonFactor/std::sqrt(-cosm.Wk) * std::sin(std::sqrt(-cosm.Wk) * result);
-	
+
 	// m_Wk > 0
 	return commonFactor/std::sqrt(cosm.Wk) * std::sinh(std::sqrt(cosm.Wk) * result);
 }
--- core/src/cosmology/cosmology.h
+++ core/src/cosmology/cosmology.h
@@ -10,23 +10,23 @@ namespace grale
 class Cosmology : public errut::ErrorBase
 {
 public:
-    Cosmology(double h = 0.7, double Wm = 0.3, double Wr = 0, double Wv = 0.7, double w = -1.0);
-    ~Cosmology();
+	Cosmology(double h = 0.7, double Wm = 0.3, double Wr = 0, double Wv = 0.7, double w = -1.0);
+	~Cosmology();
 
-    double getH() const                                                     { return m_h; }
-    double getOmegaM() const                                                { return m_Wm; }
-    double getOmegaR() const                                                { return m_Wr; }
-    double getOmegaV() const                                                { return m_Wv; }
-    double getW() const                                                     { return m_w; }
+	double getH() const                                                     { return m_h; }
+	double getOmegaM() const                                                { return m_Wm; }
+	double getOmegaR() const                                                { return m_Wr; }
+	double getOmegaV() const                                                { return m_Wv; }
+	double getW() const                                                     { return m_w; }
 
-    double getAngularDiameterDistance(double z1, double z2 = 0) const;
+	double getAngularDiameterDistance(double z1, double z2 = 0) const;
 
-    bool write(serut::SerializationInterface &si) const;
-    bool read(serut::SerializationInterface &si);
+	bool write(serut::SerializationInterface &si) const;
+	bool read(serut::SerializationInterface &si);
 private:
 	static double integrationFunction(double R, void *params);
 
-    double m_h, m_Wm, m_Wr, m_Wv, m_w;
+	double m_h, m_Wm, m_Wr, m_Wv, m_w;
 };
 
 } // end namespace
