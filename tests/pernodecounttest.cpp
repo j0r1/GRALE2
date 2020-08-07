ec85c8440041d19d8a01baf26a0b0b9c97ad3c29
Jori Liesenborgs <jori.liesenborgs@gmail.com>
2020-08-07 20:47:49 +0200

    Same spaces/tab issue, vscode detected it wrong

--- tests/pernodecounttest.cpp
+++ tests/pernodecounttest.cpp
@@ -9,28 +9,28 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    if (argc != 3)
-    {
-        cerr << "Specify file name and delay (seconds)" << endl;
-        return -1;
-    }
+	if (argc != 3)
+	{
+		cerr << "Specify file name and delay (seconds)" << endl;
+		return -1;
+	}
 
-    PerNodeCounter counter(argv[1]);
-    int delay = atoi(argv[2]);
-    if (delay < 0 || delay > 1000)
-    {
-        cerr << "Invalid delay " << delay << endl;
-        return -1;
-    }
+	PerNodeCounter counter(argv[1]);
+	int delay = atoi(argv[2]);
+	if (delay < 0 || delay > 1000)
+	{
+		cerr << "Invalid delay " << delay << endl;
+		return -1;
+	}
 
-    int num = counter.getCount();
-    if (num < 0)
-    {
-        cerr << "Error: " << counter.getErrorString() << endl;
-        return -1;
-    }
-    cout << "Count: " << num << endl;
-    cout << "Sleeping " << delay << " seconds" << endl;
-    sleep(delay);
-    return 0;
+	int num = counter.getCount();
+	if (num < 0)
+	{
+		cerr << "Error: " << counter.getErrorString() << endl;
+		return -1;
+	}
+	cout << "Count: " << num << endl;
+	cout << "Sleeping " << delay << " seconds" << endl;
+	sleep(delay);
+	return 0;
 }
