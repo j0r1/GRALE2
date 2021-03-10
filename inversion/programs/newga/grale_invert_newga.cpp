0baf41d22fe1f89f21d5244c06b039c89ecdd53c
Jori Liesenborgs <jori.liesenborgs@gmail.com>
2021-03-10 11:48:27 +0100

    Starting program to interface with new GA code

---
+++ inversion/programs/newga/grale_invert_newga.cpp
@@ -0,0 +1,75 @@
+#include "log.h"
+#include "inputoutput.h"
+#include "gaparameters.h"
+#include "lensinversiongafactoryparamssingleplanecpu.h"
+#include "inversioncommunicator.h"
+#include <errut/booltype.h>
+#include <serut/memoryserializer.h>
+#include <mogal/geneticalgorithm.h>
+#ifndef WIN32
+#include <fcntl.h>
+#endif // !WIN32
+
+#include <iostream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+using namespace grale;
+using namespace serut;
+using namespace mogal;
+using namespace errut;
+
+// TODO: rename this, is from copy-paste
+class SingleCoreCommunicator : public InversionCommunicator
+{
+public:
+	SingleCoreCommunicator() { }
+	~SingleCoreCommunicator() { }
+protected:
+	string getVersionInfo() const override { return "Single core genetic algorithm engine"; }
+
+	bool_t runGA(int popSize, GAFactory &factory, GeneticAlgorithmParams &params,
+	             const std::string &moduleDir, const std::string &moduleFile,
+	             const std::vector<uint8_t> &factoryParamBytes) override
+	{
+
+		return "TODO";
+	}
+
+//	const mogal::GeneticAlgorithm *getGeneticAlgorithm() const override { return &m_ga; }
+//private:
+//	CommonGA<GeneticAlgorithm> m_ga;
+};
+
+int main(int argc, char *argv[])
+{
+	LOG.init(argv[0]);
+#ifndef WIN32
+	// This was added to be able to launch instance in gdb, in which case
+	// stdin/stdout can no longer be used for communication with the
+	// executable (needed to control gdb).
+	if (argc == 3)
+	{
+		string inputPipeName(argv[1]);
+		string outputPipeName(argv[2]);
+		cerr << "Opening " << outputPipeName << " for communication" << endl;
+		// Nasty: override the file desc for communication
+		stdOutFileDescriptor = open(outputPipeName.c_str(), O_WRONLY); 
+		cerr << "Opening " << inputPipeName << " for communication" << endl;
+		// Nasty: override the file desc for communication
+		stdInFileDescriptor = open(inputPipeName.c_str(), O_RDWR); 
+	}
+#endif // WIN32
+
+	SingleCoreCommunicator comm;
+
+	bool_t r = comm.run();
+	if (!r)
+	{
+		cerr << "ERROR: " << r.getErrorString() << endl;
+		return -1;
+	}
+
+	return 0;
+}
