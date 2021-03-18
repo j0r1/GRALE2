62a2f8362aaac98c711b85baf8da2626f2f5edd2
Jori Liesenborgs <jori.liesenborgs@gmail.com>
2021-03-18 20:56:37 +0100

    Building old algorithm with new library

--- inversion/programs/newga/grale_invert_newga.cpp
+++ inversion/programs/newga/grale_invert_newga.cpp
@@ -13,6 +13,7 @@
 #include <mogal2/vectorgenomefitness.h>
 #include <mogal2/singlethreadedpopulationfitnesscalculation.h>
 #include <mogal2/stopcriterion.h>
+#include <mogal2/simplesortedpopulation.h>
 #ifndef WIN32
 #include <fcntl.h>
 #endif // !WIN32
@@ -44,6 +45,8 @@ public:
 			for (size_t i = 0 ; i < m_fitnesses.size() ; i++)
 				c->m_fitnesses[i] = m_fitnesses[i];
 			c->m_scaleFactor = m_scaleFactor;
+			if (isCalculated())
+				c->setCalculated();
 		}
 		return c;
 	}
@@ -219,9 +222,118 @@ private:
 	const mogal::RandomNumberGenerator *m_pRng;
 };
 
+class LensGenomeCrossover : public mogal2::GenomeCrossover
+{
+public:
+	LensGenomeCrossover(const shared_ptr<mogal2::RandomNumberGenerator> &rng, bool allowNegative)
+		: m_rng(rng), m_allowNegative(allowNegative)
+	{
+	}
+
+	LensGenomeCrossover()
+	{
+	}
+
+	errut::bool_t check(const std::vector<std::shared_ptr<mogal2::Genome>> &parents)
+	{
+		if (parents.size() != 2)
+			return "Expecting two parents";
+		if (dynamic_cast<LensGenome*>(parents[0].get()) == 0)
+			return "Genome is of wrong type";
+		return true;
+	}
+
+	errut::bool_t generateOffspring(const std::vector<std::shared_ptr<mogal2::Genome>> &parents,
+	                                        std::vector<std::shared_ptr<mogal2::Genome>> &generatedOffspring)
+	{
+		assert(parents.size() == 2);
+		LensGenome *pParents[2] = {
+			static_cast<LensGenome*>(parents[0].get()),
+			static_cast<LensGenome*>(parents[1].get())
+		};
+
+		size_t numBasisFunctions = pParents[0]->m_weights.size();
+
+		generatedOffspring.clear();
+		auto offspring = parents[0]->createCopy(false);
+		generatedOffspring.push_back(offspring);
+		LensGenome *pOff = static_cast<LensGenome*>(offspring.get());
+
+		std::vector<float> &newBasisFunctionsWeights = pOff->m_weights;
+
+		auto pickParent = [pParents, this]()
+		{
+			float x = (float)m_rng->getRandomDouble();
+			const LensGenome *pParent = pParents[(x < 0.5f)?1:0];
+			return pParent;
+		};
+
+		auto genomeUniformCrossover = [&newBasisFunctionsWeights, numBasisFunctions, &pickParent](auto check)
+		{
+			for (int i = 0 ; i < numBasisFunctions ; i++)
+			{
+				const LensGenome *pParent = pickParent();
+				newBasisFunctionsWeights[i] = pParent->m_scaleFactor * pParent->m_weights[i];
+				check(newBasisFunctionsWeights[i]);
+			}
+		};
+
+		auto noChange = [](float &x) { };
+		auto clamp = [](float &x) { if (x < 0) x = 0; };
+
+		if (m_allowNegative)
+			genomeUniformCrossover(noChange);
+		else
+			genomeUniformCrossover(clamp);
+
+		vector<float> &newSheetValues = pOff->m_sheets;
+		for (size_t i = 0 ; i < newSheetValues.size() ; i++)
+			newSheetValues[i] = pickParent()->m_sheets[i];
+
+		return true;
+	}
+private:
+	shared_ptr<mogal2::RandomNumberGenerator> m_rng;
+	bool m_allowNegative;
+};
+
+class LensFitnessComparison : public mogal2::FitnessComparison
+{
+public:
+	LensFitnessComparison() { }
+	~LensFitnessComparison() { }
+	
+	errut::bool_t check(const mogal2::Fitness &f) const
+	{
+		if (!dynamic_cast<const LensFitness*>(&f))
+			return "Fitness is of wrong type";
+		return true;
+	}
+
+	bool isFitterThan(const mogal2::Fitness &first, const mogal2::Fitness &second, size_t objectiveNumber) const
+	{
+		const LensFitness &f1 = static_cast<const LensFitness &>(first);
+		const LensFitness &f2 = static_cast<const LensFitness &>(second);
+
+		assert(f1.m_fitnesses.size() == f2.m_fitnesses.size());
+		assert(objectiveNumber < f1.m_fitnesses.size());
+
+		return f1.m_fitnesses[objectiveNumber] < f2.m_fitnesses[objectiveNumber];
+	}
+};
+// TODO: this is for a single fitness measure
 class MyCrossover : public mogal2::PopulationCrossover
 {
 public:
+	MyCrossover(double beta, bool elitism, bool includeBest, double crossoverRate,
+				const shared_ptr<mogal2::RandomNumberGenerator> &rng,
+				bool allowNegative)
+		: m_beta(beta), m_bestWithoutMutation(elitism), m_bestWithMutation(includeBest), m_crossoverRate(crossoverRate),
+		  m_rng(rng), m_sortedPop(make_shared<LensFitnessComparison>()), m_cross(rng, allowNegative)
+	{
+
+	}
+
 	errut::bool_t check(const std::shared_ptr<mogal2::Population> &population)
 	{
 		if (population->size() == 0)
@@ -231,13 +343,91 @@ public:
 			return "Genome is of wrong type";
 		if (!dynamic_cast<const LensFitness *>(i->fitnessPtr()))
 			return "Fitness is of wrong type";
+
+		bool_t r;
+		vector<shared_ptr<mogal2::Genome>> testParents = { population->individual(0)->genome(), population->individual(0)->genome() };
+		if (!(r = m_cross.check(testParents)))
+			return "Error in genome crossover check: " + r.getErrorString();
 		return true;
 	}
 
 	errut::bool_t createNewPopulation(size_t generation, std::shared_ptr<mogal2::Population> &population, size_t targetPopulationSize)
 	{
-		return "TODO: MyCrossover";
+		bool_t r;
+		if (generation == 0)
+		{
+			if (!(r = m_sortedPop.check(*population)))
+				return "Error checking sorter: " + r.getErrorString();
+		}
+
+		// TODO: do this in another function?
+		// Scale factor is calculated and stored in fitness, copy it back to genome
+		for (auto &i : population->individuals())
+		{
+			auto &g = static_cast<LensGenome &>(i->genomeRef());
+			auto &f = static_cast<LensFitness &>(i->fitnessRef());
+			g.m_scaleFactor = f.m_scaleFactor;
+		}
+
+		if (population->size() != targetPopulationSize)
+			return "Expecting size " + to_string(targetPopulationSize) + " but got " + to_string(population->size());
+		
+		if (!(r = m_sortedPop.processPopulation(population, targetPopulationSize)))
+			return "Error sorting population: " + r.getErrorString();
+
+		shared_ptr<mogal2::Population> newPop = make_shared<mogal2::Population>();
+		size_t mutOffset = 0;
+		if (m_bestWithoutMutation)
+		{
+			mutOffset = 1;
+			newPop->append(population->individual(0)->createCopy());
+		}
+		if (m_bestWithMutation)
+			newPop->append(population->individual(0)->createCopy());
+		
+		auto pickParent = [&population, this]()
+		{
+			double x = m_rng->getRandomDouble();
+			double val = (1.0-std::pow(x, 1.0/(1.0+m_beta)))*((double)population->size());
+			int r = (int)val;
+			if (r >= (int)population->size())
+					r = (int)population->size() - 1;
+			return r;
+		};
+
+		vector<shared_ptr<mogal2::Genome>> parents(2);
+		vector<shared_ptr<mogal2::Genome>> offspring;
+		while (newPop->size() < targetPopulationSize)
+		{
+			if (m_rng->getRandomDouble() < m_crossoverRate)
+			{
+				// TODO: in original version, an attempt was made to prevent inbreeding
+				for (size_t i = 0 ; i < parents.size() ; i++)
+					parents[i] = population->individual(pickParent())->genome();
+				
+				if (!(r = m_cross.generateOffspring(parents, offspring)))
+					return "Error in crossover: " + r.getErrorString();
+
+				for (auto &g : offspring)
+					newPop->append(make_shared<mogal2::Individual>(g, population->individual(0)->fitness()->createCopy(false)));
+			}
+			else // clone
+			{
+				newPop->append(population->individual(pickParent())->createCopy());
+			}
+		}
+
+		// TODO: Mutation
+
+		swap(population, newPop);
+		
+		return true;
 	}
+
+	shared_ptr<mogal2::RandomNumberGenerator> m_rng;
+	mogal2::SimpleSortedPopulation m_sortedPop;
+	LensGenomeCrossover m_cross;
+	double m_beta, m_bestWithMutation, m_bestWithoutMutation, m_crossoverRate;
 };
 
 class LensFitnessCalculation : public mogal2::GenomeFitnessCalculation
@@ -300,11 +490,19 @@ protected:
 	{
 		bool_t r;
 		grale::LensInversionGAFactoryCommon &gaFactory = dynamic_cast<grale::LensInversionGAFactoryCommon&>(factory);
+		if (gaFactory.getNumberOfFitnessComponents() != 1)
+			return "Currently only for one fitness component";
+
 		shared_ptr<RNG> rng = make_shared<RNG>(gaFactory.getRandomNumberGenerator());
 		MyGA ga;
 
 		Creation creation(gaFactory);
-		MyCrossover cross;
+		MyCrossover cross(params.getBeta(),
+					      params.useElitism(),
+						  params.alwaysIncludeBestGenome(),
+						  params.getCrossOverRate(),
+						  rng,
+						  gaFactory.allowNegativeValues());
 		mogal2::SingleThreadedPopulationFitnessCalculation calc(make_shared<LensFitnessCalculation>(gaFactory));
 		mogal2::FixedGenerationsStopCriterion stop(10); // for testing
 
