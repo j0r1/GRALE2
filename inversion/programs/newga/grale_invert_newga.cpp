9b771a2858b0853e62b72323a96a3cb19b333e17
Jori Liesenborgs <jori.liesenborgs@gmail.com>
2021-03-22 20:05:26 +0100

    Added same preferred genome selector from non dominated set

--- inversion/programs/newga/grale_invert_newga.cpp
+++ inversion/programs/newga/grale_invert_newga.cpp
@@ -156,6 +156,68 @@ public:
 	}
 };
 
+class PreferredIndividualSelector
+{
+public:
+	PreferredIndividualSelector() { }
+	virtual ~PreferredIndividualSelector() { }
+	virtual errut::bool_t select(const std::vector<std::shared_ptr<mogal2::Individual>> &best,
+								 std::shared_ptr<mogal2::Individual> &selected)
+	{
+		return "Not implemented";
+	}
+};
+
+class SubsequentBestIndividualSelector : public PreferredIndividualSelector
+{
+public:
+	SubsequentBestIndividualSelector(size_t numObjectives,
+									 const std::shared_ptr<mogal2::FitnessComparison> &fitComp)
+		: m_numObjectives(numObjectives), m_cmp(fitComp) { }
+	~SubsequentBestIndividualSelector() { }
+	
+	errut::bool_t select(const std::vector<std::shared_ptr<mogal2::Individual>> &bestIndividuals,
+								 std::shared_ptr<mogal2::Individual> &selected) override
+	{
+		vector<shared_ptr<mogal2::Individual>> genomes = bestIndividuals;
+		vector<shared_ptr<mogal2::Individual>> genomes2;
+		shared_ptr<mogal2::Individual> best;
+
+		if (genomes.size() == 0)
+			return "No best individuals to select one from";
+
+		for (size_t comp = 0 ; comp < m_numObjectives ; comp++)
+		{
+			best = genomes[0];
+
+			for (auto &i : genomes)
+			{
+				if (m_cmp->isFitterThan(i->fitnessRef(), best->fitnessRef(), comp))
+					best = i;
+			}
+
+			genomes2.clear();
+
+			// Ok, now we know a genome that has the lowest 'comp' fitness, let's see it there
+			// are others which perfom equally well
+			for (auto &i : genomes)
+			{
+				// if 'best' is not fitter than 'i' it must have the same fitness with respect to this component
+				if (!m_cmp->isFitterThan(best->fitnessRef(), i->fitnessRef(), comp))
+					genomes2.push_back(i);
+			}
+
+			swap(genomes, genomes2);
+			genomes2.clear();
+		}
+		selected = best;
+		return true;
+	}
+private:
+	size_t m_numObjectives;
+	std::shared_ptr<mogal2::FitnessComparison> m_cmp;
+};
+
 // TODO: rename this, is from copy-paste
 class SingleCoreCommunicator : public InversionCommunicator
 {
@@ -175,9 +237,18 @@ protected:
 	{
 		if (m_best.size() == 0)
 			return nullptr;
+		if (!m_selector.get())
+			return nullptr;
 		
-		// TODO: select best!
-		return m_best[0];
+		bool_t r;
+		shared_ptr<mogal2::Individual> selected;
+		if (!(r = m_selector->select(m_best, selected)))
+		{
+			cerr << "Error in preferred individual selection: " << r.getErrorString() << endl;
+			return nullptr;
+		}
+
+		return selected;
 	}
 
 	bool_t runGA(int popSize, mogal::GAFactory &factory, mogal::GeneticAlgorithmParams &params,
@@ -186,8 +257,12 @@ protected:
 	{
 		bool_t r;
 		grale::LensInversionGAFactoryCommon &gaFactory = dynamic_cast<grale::LensInversionGAFactoryCommon&>(factory);
-		if (gaFactory.getNumberOfFitnessComponents() != 1)
+		size_t numObjectives = gaFactory.getNumberOfFitnessComponents();
+		if (numObjectives != 1)
 			return "Currently only for one fitness component";
+
+		m_selector = make_shared<SubsequentBestIndividualSelector>(numObjectives,
+								make_shared<grale::LensGAFitnessComparison>());
 
 		shared_ptr<RNG> rng = make_shared<RNG>();
 		MyGA ga;
@@ -260,7 +335,9 @@ protected:
 
 		return true;
 	}
+	
 	std::vector<std::shared_ptr<mogal2::Individual>> m_best;
+	shared_ptr<SubsequentBestIndividualSelector> m_selector;
 };
 
 int main(int argc, char *argv[])
