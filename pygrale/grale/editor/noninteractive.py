343e814755a8c74b81421da0baf7bf1a95717a56
Jori Liesenborgs <jori.liesenborgs@gmail.com>
2023-03-26 12:58:42 +0200

    Cleanig up, using SceneViewDescription in foreground

--- pygrale/grale/editor/noninteractive.py
+++ pygrale/grale/editor/noninteractive.py
@@ -6,14 +6,150 @@ import pprint
 import multiprocessing
 import checkqt
 from multiprocessing import Process, Queue
+import copy
+import os
+
+class SceneViewDesciptionException(Exception):
+    pass
+
+class SceneViewDesciption(object):
+    def __init__(self, json_obj_fn = None):
+        if json_obj_fn is None:
+            self.clear()
+            return
+
+        if type(json_obj_fn) == dict:
+            self.loadObject(json_obj_fn)
+        elif os.path.exists(json_obj_fn):
+            self.loadFile(json_obj_fn)
+        else:
+            self.loadJson(json_obj_fn)
+
+    def clear(self):
+        self.activeLayerIndex = -1
+
+        self.globalSettings = {
+            "axisleft": False,
+            "ispointsizefixed": True,
+            "nextmatchpointfits": 0,
+            "nextmatchpointrgb": 0,
+            "pointsizearcsecsize": 1,
+            "pointsizepixelsize": 16,
+            "showaxes": True,
+            "showmatchpoints": True
+        }
+        
+        self.layers = []
+        self.visibilities = []
+
+        self.view = { "center": [ 0.0, 0.0 ], "zoom": 1.0 }
+
+    def loadFile(self, fn):
+        obj = json.load(open(fn, "rt"))
+        self.loadObject(obj)
+
+    def loadJson(self, jsonText):
+        obj = json.loads(jsonText)
+        self.loadObject(obj)
+
+    def loadObject(self, obj):
+        self.clear()
+
+        if "view" in obj:
+            self.view["zoom"] = float(obj["view"]["zoom"])
+            self.view["center"] = [ float(obj["view"]["center"][0]), float(obj["view"]["center"][1]) ]
+        
+        layersToAdd = None
+        visibilitiesToAdd = None
+
+        if "state" in obj:
+            layersToAdd = obj["state"]["layers"] # the 'addLayers' function will make the copy
+            visibilitiesToAdd = obj["state"]["visibilities"]
+
+            if len(visibilitiesToAdd) != len(layersToAdd):
+                raise SceneViewDesciptionException("Number of visibilities doesn't match number of layers")
+
+            self.activeLayerIndex = int(obj["state"]["activelayerindex"])
+
+            gbs = obj["state"]["globalsettings"]
+            self.globalSettings["axisleft"] = bool(gbs["axisleft"])
+            self.globalSettings["ispointsizefixed"] = bool(gbs["ispointsizefixed"])
+            self.globalSettings["nextmatchpointfits"] = int(gbs["nextmatchpointfits"])
+            self.globalSettings["nextmatchpointrgb"] = int(gbs["nextmatchpointrgb"])
+            self.globalSettings["pointsizearcsecsize"] = float(gbs["pointsizearcsecsize"])
+            self.globalSettings["pointsizepixelsize"] = int(gbs["pointsizepixelsize"])
+            self.globalSettings["showaxes"] = bool(gbs["showaxes"])
+            self.globalSettings["showmatchpoints"] = bool(gbs["showmatchpoints"])
+
+        else: # Assume it's layers only
+            layersToAdd = [ l for i in obj ] # the 'addLayers' function will make the copy
+            visibilitiesToAdd = [ True for _ in layersToAdd ] # Set everything to visible
+
+        self.addLayers(layersToAdd, visibilitiesToAdd)
+
+    def addLayers(self, layers, visibilities):
+        if len(layers) != len(visibilities):
+            raise SceneViewDesciptionException("Not the same number of layers and visibilities")
+
+        for l, v in zip(layers, visibilities):
+            self.addLayer(l, v)
+
+    def addLayer(self, layer, visibility = True):
+        # Note: if we run this as check, we need to have at least a QGuiApplication, and the
+        #       idea is to only need that in a background process
+        # Layer.fromSettings(layer) # For now we can't
+
+        # Do other elementary checks:
+        checkTd = False
+        if "rgbfile" in layer:
+            if not "transform" in layer:
+                raise SceneViewDesciptionException("Expecting 'transform' in RGB layer")
+        elif "fitsfile" in layer:
+            for k in [ "hduidx", "center", "minma" ]:
+                if not k in layer:
+                    raise SceneViewDesciptionException("Expecting '{}' in FITS layer".format(k))
+        else:
+            if not "triangles" in layer:
+                raise SceneViewDesciptionException("Expecting a 'triangles' entry in points layer")
+            checkTd = True
+
+        # Check name
+        if not "name" in layer:
+            raise Exception("Expecting a 'name' for the layer")
+
+        for pt in layer["points"][:1]: # TODO: to save time, we'll only check the first point?
+            if not "label" in pt:
+                raise Exception("Expecting a 'label' for each point")
+            if not "xy" in pt:
+                raise Exception("Exception a 'xy' coordinate for each point")
+            if checkTd and not "timedelay" in pt:
+                raise Exception("Expecting a 'timedelay' entry for each point")
+
+        self.layers.append(copy.deepcopy(layer))
+        self.visibilities.append(bool(visibility))
+
+    def toObject(self):
+        obj = {
+            "state": {
+                "activelayerindex": self.activeLayerIndex,
+                "globalsettings": copy.deepcopy(self.globalSettings),
+                "layers": copy.deepcopy(self.layers),
+                "visibilities": copy.deepcopy(self.visibilities)
+            },
+            "view": copy.deepcopy(self.view)
+        }
+        return obj
 
 class MultipleLayerScene(scenes.LayerScene):
 
-    def __init__(self):
+    def __init__(self, sceneViewDesc = None):
         super(MultipleLayerScene, self).__init__()
 
         self.layersAndWidgets = []
         self.nextZ = 0
+
+        if sceneViewDesc:
+            self.loadSceneViewDescription(sceneViewDesc)
 
     def addLayer(self, l):
         w = l.createGraphicsItem()
@@ -21,96 +157,76 @@ class MultipleLayerScene(scenes.LayerScene):
 
         self.nextZ += 1
         w.setZValue(self.nextZ)
-        return w
+        self.addItem(w)
+        w.updatePoints()
 
-    def getLayerItem(self, uuid):
-        if uuid in self.layerItems:
-            return self.layerItems[uuid]
-        raise Exception("Specified layer {} not found".format(uuid))
+#    def getLayerItem(self, uuid):
+#        if uuid in self.layerItems:
+#            return self.layerItems[uuid]
+#        raise Exception("Specified layer {} not found".format(uuid))
 
     def getCurrentItemAndLayer(self):
         return self.layersAndWidgets[-1]
 
-def importLayers(scene, layers, visibilities):
-    if visibilities is None:
-        visibilities = [ True for _ in layers ]
+    def loadSceneViewDescription(self, svd):
+        d = svd.globalSettings
+        self.setAxesVisible(d["showaxes"])
+        self.setAxisLeft(d["axisleft"])
+        self.setPointSizePixelSize(d["pointsizepixelsize"])
+        self.setPointSizeSceneSize(d["pointsizearcsecsize"])
+        self.setPointSizeFixed(d["ispointsizefixed"])
+        self.setMatchPointsVisible(d["showmatchpoints"])
 
-    for layer,visible in zip(layers,visibilities):
-        if not visible:
-            continue
+        scale = svd.view["zoom"]
+        self.onScaleChanged(scale) # May be needed to recalculate the point size transformation
 
-        l = Layer.fromSettings(layer)
-        #pprint.pprint(l)
-        w = scene.addLayer(l)
-        scene.addItem(w)
-        w.updatePoints()
+        for layer,visible in zip(svd.layers, svd.visibilities):
+            if visible:
+                self.addLayer(Layer.fromSettings(layer))
 
-def setGlobalSettings(scene, d):
-    scene.setAxesVisible(d["showaxes"])
-    scene.setAxisLeft(d["axisleft"])
-    scene.setPointSizePixelSize(d["pointsizepixelsize"])
-    scene.setPointSizeSceneSize(d["pointsizearcsecsize"])
-    scene.setPointSizeFixed(d["ispointsizefixed"])
-    scene.setMatchPointsVisible(d["showmatchpoints"])
-
-def setViewSettings(view, v):
-    ctr = v["center"]
-    s = v["zoom"]
-    view.centerOn(ctr[0], ctr[1])
-    view.setScale(s)
-
-def _loadFile(fileName):
-    from PyQt5 import QtWidgets, QtCore, QtGui
-
-    app = QtWidgets.QApplication(sys.argv)
-    scene = MultipleLayerScene()
-    #view = GraphicsView(scene)
-    
-    obj = json.load(open(fileName, "rt"))
-    if "state" in obj:
-        layers = obj["state"]["layers"]
-        visibilities = obj["state"]["visibilities"]
-        setGlobalSettings(scene, obj["state"]["globalsettings"])
-    else:
-        layers = obj
-        visibilities = None
-
-    scale = 1 if not "view" in obj else obj["view"]["zoom"]
-    scene.onScaleChanged(scale) # May be needed to recalculate the point size transformation
-
-    importLayers(scene, layers, visibilities)
-
-    return app, scene
-
-def _helper_getSceneRegionImageNumPy(q1, q2):
-    fileName, bottomLeft, topRight, widthPixels, heightPixels, grayScale = q1.get()
+def _background_helper(q1, q2, func):
+    params = q1.get()
+    svd = SceneViewDesciption(params[0])
+    params = params[1:]
 
     try:
-        app, scene = _loadFile(fileName)
-        res = scene.getSceneRegionNumPyArray(bottomLeft, topRight, widthPixels, heightPixels, grayScale)
-        #print(res)
+        from PyQt5 import QtWidgets, QtCore, QtGui
+        app = QtWidgets.QApplication(sys.argv)
+
+        res = func(svd, params)
         q2.put([True, res])
     except Exception as e:
         q2.put([False, str(e)])
 
-# Coords are in arcsec for now
-def getSceneRegionNumPyArray(fileName, bottomLeft, topRight, widthPixels = None, heightPixels = None, grayScale = False):
+def _background_helper_getSceneRegionImageNumPy(q1, q2):
+    _background_helper(q1, q2, lambda svd, params: MultipleLayerScene(svd).getSceneRegionNumPyArray(*params))
+
+def _foreground_helper(helperFunction, paramList):
     checkqt.checkQtAvailable()
 
     q1, q2 = Queue(), Queue()
-    p = Process(target=_helper_getSceneRegionImageNumPy, args=(q1,q2))
+    p = Process(target=helperFunction, args=(q1,q2))
     p.start()
-    q1.put([fileName, bottomLeft, topRight, widthPixels, heightPixels, grayScale ])
+    q1.put(paramList)
     flag, result = q2.get()
     p.join()
     if not flag:
         raise Exception(result)
     return result
+
+# Coords are in arcsec for now
+def getSceneRegionNumPyArray(sceneViewDesc, bottomLeft, topRight, widthPixels = None, heightPixels = None, grayScale = False):
+    return _foreground_helper(_background_helper_getSceneRegionImageNumPy,
+                              [ sceneViewDesc.toObject(), bottomLeft, topRight, widthPixels, heightPixels, grayScale ])
     
 def main():
 
-    img = getSceneRegionNumPyArray("cl0024.json", [ -10, -10 ], [10, 10], 512)
+    #svd = SceneViewDesciption("cl0024.json")
+    #pprint.pprint(svd.toObject())
 
+    svd = SceneViewDesciption("cl0024.json")
+    img = getSceneRegionNumPyArray(svd, [ -10, -10 ], [10, 10], 512)
+    
     import matplotlib.pyplot as plt
     plt.imshow(img)
     plt.show()
