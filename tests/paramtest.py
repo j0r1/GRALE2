42bcac2225ffe620219b164794e670ca1d5fd1d6
Jori Liesenborgs <jori.liesenborgs@gmail.com>
2024-10-12 23:05:25 +0200

    Getting min / max initial value range; sanity checks

--- tests/paramtest.py
+++ tests/paramtest.py
@@ -1,6 +1,7 @@
 from grale.all import *
 import sys
 import pprint
+import copy
 
 def getInitialParameterValue(value):
     if type(value) == dict:
@@ -13,11 +14,13 @@ def getInitialParameterValue(value):
     
     return value, True
 
-def getMinOrMaxParameterValue(value, fraction, keyPrefix, isMin):
+def getMinOrMaxParameterValue(value, fraction, keyPrefix, isMin, allowMissingKey):
     key = keyPrefix + "min" if isMin else keyPrefix + "max"
     fracSign = -1 if isMin else 1
 
     if type(value) == dict:
+        if not key in value and allowMissingKey: # for the hard limits
+            return None, False
         return value[key], False
     
     if type(value) == list or type(value) == tuple:
@@ -109,10 +112,79 @@ def createTemplateLens_helper(parametricLensDescription, Dd, getParamValue):
     else:
         raise Exception("Unknown lens type for parametric inversion")
 
+def _createParamOffsetInfo(l, paramNames):
+    adjustableParams = l.getCLAdjustableFloatingPointParameterInfo()
+    adjustableParamsDict = { }
+    for p in adjustableParams:
+        name = p["name"]
+        if name in adjustableParamsDict:
+            raise Exception(f"Internal error: name {name} already exists in adjustable params dict")
+        adjustableParamsDict[name] = p
+
+    paramOffsetInfo = []    
+    for n in paramNames:
+        if not n in adjustableParamsDict:
+            raise Exception(f"Internal error: specified adjustable param {n} does not seem to be valid")
+        paramOffsetInfo.append(adjustableParamsDict[n])
+
+    return sorted(paramOffsetInfo, key=lambda x: x["offset"])
+
 def createTemplateLens(parametricLensDescription, Dd):
 
-    return createTemplateLens_helper(parametricLensDescription, Dd, getInitialParameterValue)
+    l, paramNames = createTemplateLens_helper(parametricLensDescription, Dd, getInitialParameterValue)
+    scales = l.getSuggestedScales()
+    intParam, floatParams = l.getCLParameters(**scales)
+    ret = { "templatelens": l,
+            "paramoffsets": _createParamOffsetInfo(l, paramNames),
+            "scales": scales,
+            "floatparams": floatParams,
+            "description": copy.deepcopy(parametricLensDescription)
+          }
+    return ret
+
+def createInitialMinMaxParameters(templateLensDesciption, defaultFraction):
     
+    scales = templateLensDesciption["scales"]
+    Dd = templateLensDesciption["templatelens"].getLensDistance()
+    parametricLensDescription = templateLensDesciption["description"]
+    
+    l, paramNames = createTemplateLens_helper(parametricLensDescription, Dd,
+                                              lambda value: getMinOrMaxParameterValue(value, defaultFraction, "init", True, False))
+    _, initMinParams = l.getCLParameters(**scales)
+
+    l, paramNames = createTemplateLens_helper(parametricLensDescription, Dd,
+                                              lambda value: getMinOrMaxParameterValue(value, defaultFraction, "init", False, False))
+    _, initMaxParams = l.getCLParameters(**scales)
+
+    assert templateLensDesciption["floatparams"].shape == initMinParams.shape, "Internal error: mismatch in floating point parameter shapes"
+    assert initMaxParams.shape == initMinParams.shape, "Internal error: mismatch in floating point parameter shapes (2)"
+
+    numParams = initMaxParams.shape[0]
+    shouldBeSame = [ True for _ in range(numParams) ]
+    for paramInfo in templateLensDesciption["paramoffsets"]:
+        offset = paramInfo["offset"]
+        assert shouldBeSame[offset], "Internal error: changing same floating point value twice"
+
+        shouldBeSame[offset] = False
+
+    # print("paramInfo:")
+    # print(paramInfo)
+    # print("Min:")
+    # print(initMinParams)
+    # print("Max:")
+    # print(initMaxParams)
+    for idx, value in enumerate(shouldBeSame):
+        # print(f"Comparing index {idx}: {initMinParams[idx]} vs {initMaxParams[idx]}")
+        if value: # everything should be same
+            assert initMinParams[idx] == initMaxParams[idx], "Internal error: min/max should be same at this position"
+            assert initMinParams[idx] == templateLensDesciption["floatparams"][idx], "Internal error: min and template values should match at this position"
+
+        else:
+            if initMinParams[idx] == initMaxParams[idx]:
+                raise Exception(f"Values at floating point offset {idx} should be allowed to change, but initial min/max values are the same (so no variation will be introduced)")
+
+    return initMinParams, initMaxParams
+
 def main():
 
     #parametricLens = {
@@ -127,7 +199,7 @@ def main():
     parametricLens = {
        "type": "CompositeLens",
        "params": [
-            { "x": 0, "y": [ 0 ], "factor": 1, "angle": 0,
+            { "x": 0, "y": [ 0.1 ], "factor": 1, "angle": 0,
               "lens": {
                 "type": "PlummerLens",
                 "params": {
@@ -136,7 +208,7 @@ def main():
                 }
               }
             },
-            { "x": [0], "y": 0, "factor": [ 1 ], "angle": 0,
+            { "x": [0.11], "y": 0, "factor": [ 1 ], "angle": 0,
               "lens": {
                 "type": "PlummerLens",
                 "params": {
@@ -145,7 +217,8 @@ def main():
                 }
               }
             },
-            { "x": [0], "y": 0, "factor": 1, "angle": [ 0 ],
+            { "x": [0.111], "y": 0, "factor": 1, 
+              "angle": { "initmin": 0, "initmax": 3.14 },
               "lens": {
                   "type": "NSIELens",
                   "params": {
@@ -157,14 +230,14 @@ def main():
             }
         ]
     }
-    l, p = createTemplateLens(parametricLens, 1000*DIST_MPC)
-
-    print("Lens")
-    print(l)
-    print("OpenCL variable param names:")
-    pprint.pprint(p)
-    print("Adjustable parameters:")
-    pprint.pprint(l.getCLAdjustableFloatingPointParameterInfo())
+    
+    inf = createTemplateLens(parametricLens, 1000*DIST_MPC)
+    print("Template lens info")
+    pprint.pprint(inf)
+    minMax = createInitialMinMaxParameters(inf, 0.1)
+    print("initial min/max info")
+    pprint.pprint(minMax)
+    
 
 if __name__ == "__main__":
     main()
