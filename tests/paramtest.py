be4d4ff563a61e94fbb7ca9068f992bfe8ba40b0
Jori Liesenborgs <jori.liesenborgs@gmail.com>
2024-10-13 13:11:14 +0200

    Working on getting hard min/max params

--- tests/paramtest.py
+++ tests/paramtest.py
@@ -2,8 +2,10 @@ from grale.all import *
 import sys
 import pprint
 import copy
+import math
 
-def getInitialParameterValue(value):
+def getInitialParameterValue(params, paramKey):
+    value = params[paramKey]
     if type(value) == dict:
         return (value["initmin"] + value["initmax"])*0.5, False
     
@@ -14,13 +16,13 @@ def getInitialParameterValue(value):
     
     return value, True
 
-def getMinOrMaxParameterValue(value, fraction, keyPrefix, isMin, allowMissingKey):
-    key = keyPrefix + "min" if isMin else keyPrefix + "max"
+def getInitialMinOrMaxParameterValue(params, paramKey, fraction, isMin):
+    value = params[paramKey]
+
+    key = "initmin" if isMin else "initmax"
     fracSign = -1 if isMin else 1
 
     if type(value) == dict:
-        if not key in value and allowMissingKey: # for the hard limits
-            return None, False
         return value[key], False
     
     if type(value) == list or type(value) == tuple:
@@ -36,17 +38,17 @@ def getMinOrMaxParameterValue(value, fraction, keyPrefix, isMin, allowMissingKey
     return value, True
 
 def _checkParameterValues(lensParams, paramMapping, getParamValue, positionNames):
-    lensParams = lensParams.copy() # shallow copy is enough
+    remainingLensParams = lensParams.copy() # shallow copy is enough
     newParams = { }
     
     for x,y in paramMapping:
         
-        newParams[x], isFixed = getParamValue(lensParams[x])
-        del lensParams[x]
+        newParams[x], isFixed = getParamValue(lensParams, x)
+        del remainingLensParams[x]
         if not isFixed:
             positionNames.append(y)
 
-    return lensParams, newParams
+    return remainingLensParams, newParams
 
 def processPlummerLens(lensParams, Dd, getParamValue):
     positionNames = [ ]
@@ -136,6 +138,7 @@ def createTemplateLens(parametricLensDescription, Dd):
     intParam, floatParams = l.getCLParameters(**scales)
     ret = { "templatelens": l,
             "paramoffsets": _createParamOffsetInfo(l, paramNames),
+            "paramnames": paramNames, # in original order
             "scales": scales,
             "floatparams": floatParams,
             "description": copy.deepcopy(parametricLensDescription)
@@ -149,11 +152,11 @@ def createInitialMinMaxParameters(templateLensDesciption, defaultFraction):
     parametricLensDescription = templateLensDesciption["description"]
     
     l, paramNames = createTemplateLens_helper(parametricLensDescription, Dd,
-                                              lambda value: getMinOrMaxParameterValue(value, defaultFraction, "init", True, False))
+                                              lambda params, key: getInitialMinOrMaxParameterValue(params, key, defaultFraction, True))
     _, initMinParams = l.getCLParameters(**scales)
 
     l, paramNames = createTemplateLens_helper(parametricLensDescription, Dd,
-                                              lambda value: getMinOrMaxParameterValue(value, defaultFraction, "init", False, False))
+                                              lambda params, key: getInitialMinOrMaxParameterValue(params, key, defaultFraction, False))
     _, initMaxParams = l.getCLParameters(**scales)
 
     assert templateLensDesciption["floatparams"].shape == initMinParams.shape, "Internal error: mismatch in floating point parameter shapes"
@@ -173,6 +176,8 @@ def createInitialMinMaxParameters(templateLensDesciption, defaultFraction):
     # print(initMinParams)
     # print("Max:")
     # print(initMaxParams)
+
+    # TODO: move this later? Also check against hard min/max?
     for idx, value in enumerate(shouldBeSame):
         # print(f"Comparing index {idx}: {initMinParams[idx]} vs {initMaxParams[idx]}")
         if value: # everything should be same
@@ -184,6 +189,102 @@ def createInitialMinMaxParameters(templateLensDesciption, defaultFraction):
                 raise Exception(f"Values at floating point offset {idx} should be allowed to change, but initial min/max values are the same (so no variation will be introduced)")
 
     return initMinParams, initMaxParams
+
+def getHardMinOrMaxParameterValue(params, paramKey, isMin, knownParamNames, hardInfinite):
+
+    # We're expecting either a fixed value, or a dict { "start", "hardmin", "hardmax"}
+    value = params[paramKey]
+    if type(value) != dict:
+        return value, True
+
+    paramName = knownParamNames.pop(0)
+
+    key, infValue = ("hardmin", float("-inf")) if isMin else ("hardmax", float("inf"))
+    extValue = value[key]
+    if math.isinf(extValue):
+        paramValue = value["start"] # Don't use inf as a lens parameter
+        if extValue != infValue:
+            raise Exception(f"Got {extValue} but expecting {infValue} for {paramName}")
+        hardInfinite.append(paramName) # Remember this parameter name
+    else:
+        paramValue = extValue
+
+    return paramValue, False
+    
+def mergeHardMinOrMaxParameterValue(params, key, knownParamNames, paramOffsets):
+
+    # print("knownParamNames")
+    # pprint.pprint(knownParamNames)
+    # print("paramOffsets")
+    # pprint.pprint(paramOffsets)
+    startValue, isFixed = getInitialParameterValue(params, key)
+    if isFixed:
+        return startValue, True
+
+    paramName = knownParamNames.pop(0)
+    paramInfo = paramOffsets[paramName]
+
+    value = params[key]
+
+    newDict = { "start": startValue }
+
+    if type(value) == dict:
+        for pyKey, cKey in [ ("hardmin", "hard_min"), ("hardmax", "hard_max")]:
+            if pyKey in value:
+                newDict[pyKey] = value[pyKey]
+            else:
+                newDict[pyKey] = paramInfo[cKey]
+
+    else:
+        for pyKey, cKey in [ ("hardmin", "hard_min"), ("hardmax", "hard_max")]:
+            newDict[pyKey] = paramInfo[cKey]
+    
+    # Change it!
+    params[key] = newDict
+    return startValue, False
+
+def createHardMinMaxParameters(templateLensDesciption):
+
+    # We're going to merge this with hard min/max values
+    parametricLensDescription = copy.deepcopy(templateLensDesciption["description"])
+    Dd = templateLensDesciption["templatelens"].getLensDistance()
+    knownParamNames = copy.deepcopy(templateLensDesciption["paramnames"])
+    paramOffsets = templateLensDesciption["paramoffsets"]
+    paramOffsets = { x["name"]:x for x in paramOffsets }
+    scales = templateLensDesciption["scales"]
+
+    l, paramNames = createTemplateLens_helper(parametricLensDescription, Dd,
+                                              lambda params, key: mergeHardMinOrMaxParameterValue(params, key, knownParamNames, paramOffsets))
+
+    # Now parametricLensDescription is modified, to contain
+    # "start", "hardmin" and "hardmax" values for all variable
+    # entries
+
+    knownParamNames = copy.deepcopy(templateLensDesciption["paramnames"])
+    hardInfMinNames = []
+    l, _ = createTemplateLens_helper(parametricLensDescription, Dd,
+                                     lambda params, key: getHardMinOrMaxParameterValue(params, key, True, knownParamNames, hardInfMinNames))
+
+    _, hardMinParams = l.getCLParameters(**scales) # These should all be finite
+
+
+    knownParamNames = copy.deepcopy(templateLensDesciption["paramnames"])
+    hardInfMaxNames = []
+    l, _ = createTemplateLens_helper(parametricLensDescription, Dd,
+                                     lambda params, key: getHardMinOrMaxParameterValue(params, key, False, knownParamNames, hardInfMaxNames))
+
+    _, hardMaxParams = l.getCLParameters(**scales) # These should all be finite
+
+    print("hardInfMinNames:")
+    print(hardInfMinNames)
+    print("hardInfMaxNames:")
+    print(hardInfMaxNames)
+    print("hardMinParams")
+    print(hardMinParams)
+    print("hardMaxParams")
+    print(hardMaxParams)
+    raise Exception("TODO")
+
 
 def main():
 
@@ -237,8 +338,9 @@ def main():
     minMax = createInitialMinMaxParameters(inf, 0.1)
     print("initial min/max info")
     pprint.pprint(minMax)
-    
 
+    createHardMinMaxParameters(inf)
+    
 if __name__ == "__main__":
     main()
     
