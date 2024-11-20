ce4c6b28314d3fad3d86e8198d3f19330c648ce7
Jori Liesenborgs <jori.liesenborgs@gmail.com>
2024-11-20 22:56:28 +0100

    Further work on wrapper

--- inversion/src/galens/positionrandomizationbpwrapper.h
+++ inversion/src/galens/positionrandomizationbpwrapper.h
@@ -3,6 +3,7 @@
 #include "graleconfig.h"
 #include "projectedimagesinterface.h"
 #include <memory>
+#include <stdexcept>
 
 namespace grale
 {
@@ -13,61 +14,32 @@ public:
 	PositionRandomizationBackprojectWrapper(const std::shared_ptr<ProjectedImagesInterface> &baseBackProjector) : m_baseBp(baseBackProjector) { }
 	~PositionRandomizationBackprojectWrapper() { }
 
+	// TODO: set differences for sets of images
+
 	double getLensDistance() const override { return m_baseBp->getLensDistance(); }
 	double getLensRedshift() const override { return m_baseBp->getLensRedshift(); }
 	double getAngularScale() const override { return m_baseBp->getAngularScale(); };
 
-	// TODO: These should be modified
-	void checkHaveRandomization(int sourceNum) const
+	bool hasRandomization(int sourceNum) const
 	{
-		// TODO!
+		assert(sourceNum >= 0 && sourceNum < m_srcHaveUncerts.size());
+		return m_srcHaveUncerts[sourceNum];
 	}
-
+	
 	void checkHaveNoRandomization(int sourceNum) const
 	{
-		// TODO!
+		if (hasRandomization(sourceNum))
+			throw std::runtime_error("Cannot use call with image positions that have random offsets");
 	}
 
-	const Vector2D<float> *getBetas(int sourcenum) const override
-	{
-		checkHaveRandomization(sourcenum);
-		return m_baseBp->getBetas(sourcenum);
-	}
-	const Vector2D<float> *getBetas(int sourcenum, int imagenum) const override
-	{
-		checkHaveRandomization(sourcenum);
-		return m_baseBp->getBetas(sourcenum, imagenum);
-	}
-	const Vector2D<float> *getAlphas(int sourcenum) const override
-	{
-		checkHaveRandomization(sourcenum);
-		return m_baseBp->getAlphas(sourcenum);
-	}
-	const Vector2D<float> *getAlphas(int sourcenum, int imagenum) const override
-	{
-		checkHaveRandomization(sourcenum);
-		return m_baseBp->getAlphas(sourcenum, imagenum);
-	}
-	const Vector2D<float> *getThetas(int sourcenum) const override
-	{
-		checkHaveRandomization(sourcenum);
-		return m_baseBp->getThetas(sourcenum);
-	}
-	const Vector2D<float> *getThetas(int sourcenum, int imagenum) const override
-	{
-		checkHaveRandomization(sourcenum);
-		return m_baseBp->getThetas(sourcenum, imagenum);
-	}
-	const float *getLensPotential(int sourceNumber) const override
-	{
-		checkHaveRandomization(sourceNumber);
-		return m_baseBp->getLensPotential(sourceNumber);
-	}
-	const float *getLensPotential(int sourceNumber, int imageNumber) const override
-	{
-		checkHaveRandomization(sourceNumber);
-		return m_baseBp->getLensPotential(sourceNumber, imageNumber);
-	}
+	const Vector2D<float> *getBetas(int sourcenum) const override;
+	const Vector2D<float> *getBetas(int sourcenum, int imagenum) const override;
+	const Vector2D<float> *getAlphas(int sourcenum) const override;
+	const Vector2D<float> *getAlphas(int sourcenum, int imagenum) const override;
+	const Vector2D<float> *getThetas(int sourcenum) const override;
+	const Vector2D<float> *getThetas(int sourcenum, int imagenum) const override;
+	const float *getLensPotential(int sourceNumber) const override;
+	const float *getLensPotential(int sourceNumber, int imageNumber) const override;
 
 	// These should not be used with randomized positions, just forward them
 	const float *getDerivativesXX(int sourceNumber) const override { checkHaveNoRandomization(sourceNumber); return m_baseBp->getDerivativesXX(sourceNumber); }
@@ -96,10 +68,236 @@ public:
 	// TODO: remove this, or at least as virtual function, can be calculated from rest?
 	float getTimeDelay(int sourceNumber, int imageNumber, int pointNumber, Vector2D<float> beta) const override
 	{
-		assert(0);
+		throw std::runtime_error("getTimedelay is currently not supported for case with random image offsets");
 	}
 private:
+	const Vector2D<float> *getOriginalAlphas(int sourcenum) const { return m_baseBp->getAlphas(sourcenum); }
+	const Vector2D<float> *getOriginalAlphas(int sourcenum, int imagenum) const { return m_baseBp->getAlphas(sourcenum, imagenum); }
+
+	void checkAdjustedThetas(int sourceNum) const
+	{
+		assert(sourceNum >= 0 && sourceNum < m_srcAdjustedThetas.size());
+		assert(m_srcHaveUncerts[sourceNum]);
+		if (m_srcAdjustedThetas[sourceNum].size() != 0) // already calculated, reuse
+			return;
+		
+		assert(sourceNum < m_srcImagePositionDifferences.size());
+		int numPts = (int)m_srcImagePositionDifferences[sourceNum].size();
+		assert(numPts == m_baseBp->getNumberOfImagePoints(sourceNum));
+
+		const Vector2Df *pOrigThetas = m_baseBp->getThetas(sourceNum);
+		const Vector2Df *pDiffThetas = m_srcImagePositionDifferences[sourceNum].data();
+		for (size_t i = 0 ; i < numPts ; i++)
+			m_srcAdjustedThetas[sourceNum].push_back(pDiffThetas[i] + pOrigThetas[i]);
+	}
+
+	void checkAdjustedAlphas(int sourceNum) const
+	{
+		assert(sourceNum >= 0 && sourceNum < m_srcAdjustedAlphas.size());
+		assert(m_srcHaveUncerts[sourceNum]);
+		if (m_srcAdjustedAlphas[sourceNum].size() != 0) // already calculated, reuse
+			return;
+		
+		assert(sourceNum < m_srcImagePositionDifferences.size());
+		int numPts = (int)m_srcImagePositionDifferences[sourceNum].size();
+		assert(numPts == m_baseBp->getNumberOfImagePoints(sourceNum));
+
+		const Vector2Df *pOrigAlphas = m_baseBp->getAlphas(sourceNum);
+		const Vector2Df *pDiffThetas = m_srcImagePositionDifferences[sourceNum].data();
+		const float *pAxx = m_baseBp->getDerivativesXX(sourceNum);
+		const float *pAyy = m_baseBp->getDerivativesYY(sourceNum);
+		const float *pAxy = m_baseBp->getDerivativesXY(sourceNum);
+
+		for (size_t i = 0 ; i < numPts ; i++)
+		{
+			Vector2Df alpha = pOrigAlphas[i];
+			float dx = pDiffThetas[i].getX();
+			float dy = pDiffThetas[i].getY();
+			float axx = pAxx[i];
+			float ayy = pAyy[i];
+			float axy = pAxy[i];
+
+			float newAx = alpha.getX() + axx*dx + axy*dy;
+			float newAy = alpha.getY() + axy*dx + ayy*dy;
+
+			m_srcAdjustedAlphas[sourceNum].push_back({newAx, newAy});
+		}
+	}
+
+	void checkAdjustedBetas(int sourceNum) const
+	{
+		assert(sourceNum >= 0 && sourceNum < m_srcAdjustedBetas.size());
+		assert(m_srcHaveUncerts[sourceNum]);
+		if (m_srcAdjustedBetas[sourceNum].size() != 0) // already calculated, reuse
+			return;
+		
+		assert(sourceNum < m_srcImagePositionDifferences.size());
+		int numPts = (int)m_srcImagePositionDifferences[sourceNum].size();
+		assert(numPts == m_baseBp->getNumberOfImagePoints(sourceNum));
+
+		const Vector2Df *pOrigBetas = m_baseBp->getBetas(sourceNum);
+		const Vector2Df *pDiffThetas = m_srcImagePositionDifferences[sourceNum].data();
+		const float *pAxx = m_baseBp->getDerivativesXX(sourceNum);
+		const float *pAyy = m_baseBp->getDerivativesYY(sourceNum);
+		const float *pAxy = m_baseBp->getDerivativesXY(sourceNum);
+
+		for (size_t i = 0 ; i < numPts ; i++)
+		{
+			Vector2Df beta = pOrigBetas[i];
+			float dx = pDiffThetas[i].getX();
+			float dy = pDiffThetas[i].getY();
+			float axx = pAxx[i];
+			float ayy = pAyy[i];
+			float axy = pAxy[i];
+
+			float newBx = beta.getX() + (1.0f-axx)*dx - axy*dy;
+			float newBy = beta.getY() - axy*dx + (1.0f-ayy)*dy;
+
+			m_srcAdjustedBetas[sourceNum].push_back({newBx, newBy});
+		}
+	}
+
+	void checkAdjustedPotentials(int sourceNum) const
+	{
+		assert(sourceNum >= 0 && sourceNum < m_srcAdjustedPotentials.size());
+		assert(m_srcHaveUncerts[sourceNum]);
+		if (m_srcAdjustedPotentials[sourceNum].size() != 0) // already calculated, reuse
+			return;
+		
+		assert(sourceNum < m_srcImagePositionDifferences.size());
+		int numPts = (int)m_srcImagePositionDifferences[sourceNum].size();
+		assert(numPts == m_baseBp->getNumberOfImagePoints(sourceNum));
+
+		const float *pOrigPotentials = m_baseBp->getLensPotential(sourceNum);
+		const Vector2Df *pOrigAlphas = m_baseBp->getAlphas(sourceNum);
+		const Vector2Df *pDiffThetas = m_srcImagePositionDifferences[sourceNum].data();
+
+		for (size_t i = 0 ; i < numPts ; i++)
+		{
+			float phi = pOrigPotentials[i];
+			Vector2Df alpha = pOrigAlphas[i];
+			float dx = pDiffThetas[i].getX();
+			float dy = pDiffThetas[i].getY();
+
+			float newPhi = phi + alpha.getX()*dx + alpha.getY()*dy;
+			m_srcAdjustedPotentials[sourceNum].push_back(newPhi);
+		}
+	}
+
 	std::shared_ptr<ProjectedImagesInterface> m_baseBp;
+	std::vector<bool> m_srcHaveUncerts;
+	
+	std::vector<std::vector<Vector2Df>> m_srcImagePositionDifferences;
+	std::vector<std::vector<int>> m_srcImageOffsets;
+
+	mutable std::vector<std::vector<Vector2Df>> m_srcAdjustedThetas;
+	mutable std::vector<std::vector<Vector2Df>> m_srcAdjustedAlphas;
+	mutable std::vector<std::vector<Vector2Df>> m_srcAdjustedBetas;
+	mutable std::vector<std::vector<float>> m_srcAdjustedPotentials;
 };
+
+inline const Vector2D<float> *PositionRandomizationBackprojectWrapper::getThetas(int sourcenum) const
+{
+	if (hasRandomization(sourcenum))
+	{
+		checkAdjustedThetas(sourcenum);
+		return m_srcAdjustedThetas[sourcenum].data();
+	}
+	else
+		return m_baseBp->getThetas(sourcenum);
+}
+
+inline const Vector2D<float> *PositionRandomizationBackprojectWrapper::getThetas(int sourcenum, int imagenum) const
+{
+	if (hasRandomization(sourcenum))
+	{
+		checkAdjustedThetas(sourcenum);
+		assert(sourcenum < m_srcImageOffsets.size() && imagenum >= 0 && imagenum < m_srcImageOffsets[sourcenum].size());
+		int offset = m_srcImageOffsets[sourcenum][imagenum];
+		assert(offset < m_srcAdjustedThetas[sourcenum].size());
+		assert(m_srcAdjustedThetas[sourcenum].size() == m_baseBp->getNumberOfImagePoints(sourcenum));
+		return &m_srcAdjustedThetas[sourcenum][offset];
+	}
+	else
+		return m_baseBp->getThetas(sourcenum, imagenum);
+}
+
+inline const Vector2D<float> *PositionRandomizationBackprojectWrapper::getAlphas(int sourcenum) const
+{
+	if (hasRandomization(sourcenum))
+	{
+		checkAdjustedAlphas(sourcenum);
+		return m_srcAdjustedAlphas[sourcenum].data();
+	}
+	else
+		return m_baseBp->getAlphas(sourcenum);
+}
+
+inline const Vector2D<float> *PositionRandomizationBackprojectWrapper::getAlphas(int sourcenum, int imagenum) const
+{
+	if (hasRandomization(sourcenum))
+	{
+		checkAdjustedAlphas(sourcenum);
+		assert(sourcenum < m_srcImageOffsets.size() && imagenum >= 0 && imagenum < m_srcImageOffsets[sourcenum].size());
+		int offset = m_srcImageOffsets[sourcenum][imagenum];
+		assert(offset < m_srcAdjustedAlphas[sourcenum].size());
+		assert(m_srcAdjustedAlphas[sourcenum].size() == m_baseBp->getNumberOfImagePoints(sourcenum));
+		return &m_srcAdjustedAlphas[sourcenum][offset];
+	}
+	else
+		return m_baseBp->getAlphas(sourcenum, imagenum);
+}
+
+inline const Vector2D<float> *PositionRandomizationBackprojectWrapper::getBetas(int sourcenum) const
+{
+	if (hasRandomization(sourcenum))
+	{
+		checkAdjustedBetas(sourcenum);
+		return m_srcAdjustedBetas[sourcenum].data();
+	}
+	else
+		return m_baseBp->getBetas(sourcenum);
+}
+
+inline const Vector2D<float> *PositionRandomizationBackprojectWrapper::getBetas(int sourcenum, int imagenum) const
+{
+	if (hasRandomization(sourcenum))
+	{
+		checkAdjustedBetas(sourcenum);
+		assert(sourcenum < m_srcImageOffsets.size() && imagenum >= 0 && imagenum < m_srcImageOffsets[sourcenum].size());
+		int offset = m_srcImageOffsets[sourcenum][imagenum];
+		assert(offset < m_srcAdjustedBetas[sourcenum].size());
+		assert(m_srcAdjustedBetas[sourcenum].size() == m_baseBp->getNumberOfImagePoints(sourcenum));
+		return &m_srcAdjustedBetas[sourcenum][offset];
+	}
+	else
+		return m_baseBp->getBetas(sourcenum, imagenum);
+}
+
+inline const float *PositionRandomizationBackprojectWrapper::getLensPotential(int sourcenum) const
+{
+	if (hasRandomization(sourcenum))
+	{
+		checkAdjustedPotentials(sourcenum);
+		return m_srcAdjustedPotentials[sourcenum].data();
+	}
+	else
+		return m_baseBp->getLensPotential(sourcenum);
+}
+
+inline const float *PositionRandomizationBackprojectWrapper::getLensPotential(int sourcenum, int imagenum) const
+{
+	if (hasRandomization(sourcenum))
+	{
+		checkAdjustedPotentials(sourcenum);
+		assert(sourcenum < m_srcImageOffsets.size() && imagenum >= 0 && imagenum < m_srcImageOffsets[sourcenum].size());
+		int offset = m_srcImageOffsets[sourcenum][imagenum];
+		assert(offset < m_srcAdjustedPotentials[sourcenum].size());
+		assert(m_srcAdjustedPotentials[sourcenum].size() == m_baseBp->getNumberOfImagePoints(sourcenum));
+		return &m_srcAdjustedPotentials[sourcenum][offset];
+	}
+	else
+		return m_baseBp->getLensPotential(sourcenum, imagenum);
+}
 
 } // end namespace
