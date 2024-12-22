ccb71ac51e3f0c3779336316a2c94facb9b6182f
Jori Liesenborgs <jori.liesenborgs@gmail.com>
2024-12-22 23:40:47 +0100

    Updated one of the build scripts
    Can only use python3, using venv module, removed grale_editor/PyQt
    support

--- scripts/BuildAll.sh
+++ scripts/BuildAll.sh
@@ -17,9 +17,6 @@ Environment variables that are used:
 
  - NOPULL: skip git pull to update used repositories
 
- - NOQT: do not attempt to detect if Qt is available, no PyQt will
-   be installed
-
  - CMAKE_EXTRA_OPTS: extra options that are passed to each 'cmake'
    command. I use it to set 'ADDITIONAL_LIBRARIES_CORE' for GRALE2
    to be able to use the correct GSL libraries, with something like
@@ -50,6 +47,8 @@ to use the Intel compiler, while on another I need to set
 to use the correct GNU compiler enabled with a 'module load' command
 (cmake seems to prefer e.g. /usr/bin/cc otherwise).
 
+This script no longer tries to build grale_editor. If you need this,
+the conda build script will probably be easier to use.
 EOF
 }
 
@@ -59,6 +58,28 @@ if [ "$#" != 1 ] ; then
 	exit -1
 fi
 
+if ! [ -z "$PYTHON" ] ; then
+	echo "Specified python executable $PYTHON"
+	X=`"$PYTHON" --version 2>&1 |cut -f 2 -d  " "|cut -f 1 -d .`
+	if [ "$X" != 3 ] ; then
+		echo "This does not seem to be a python 3 executable, exiting"
+		exit -1
+	fi
+else
+	X=`python --version 2>&1 |cut -f 2 -d  " "|cut -f 1 -d .`
+	if [ "$X" = "3" ] ; then
+		PYTHON=python
+	else
+		X=`python3 --version 2>&1 |cut -f 2 -d  " "|cut -f 1 -d .`
+		if [ "$X" = "3" ] ; then
+			PYTHON=python3
+		else
+			echo "Unable to find python 3 executable, you can try setting PYTHON environment variable"
+			exit -1
+		fi
+	fi
+	echo "Using python executable '$PYTHON'"
+fi
 
 if ! git --version ; then
 	echo "Need git"
@@ -88,35 +109,36 @@ fi
 
 PREFIX=`pwd`
 if [ -z "$NUMCORES" ] ; then
-	NUMCORES=`python -c "import multiprocessing;print(multiprocessing.cpu_count())"`
+	NUMCORES=`"$PYTHON" -c "import multiprocessing;print(multiprocessing.cpu_count())"`
 fi
 
 export PATH="$PREFIX/bin:$PATH"
 
-if pip3 --version ; then
-	PIP=pip3
-elif pip --version ; then
-	PIP=pip
-else
-	echo "Couldn't detect pip, aborting"
-	exit -1
-fi
-
 if [ -z "$NOVENV" ] ; then
 
 	if ! [ -e "$PREFIX/bin/activate" ] ; then
-		if ! virtualenv --version ; then
-			echo "Virtualenv not found, installing"
-			$PIP install --cache-dir "$D/pipcache" virtualenv --prefix="$PREFIX" -I
-			export PYTHONPATH=`echo $PREFIX/lib/*/site-packages/`
-			echo $PYTHONPATH
-		fi
-		virtualenv --always-copy "$PREFIX"
+		"$PYTHON" -m venv --copies "$PREFIX"
 	fi
 	source "$PREFIX/bin/activate"
+
+	if ! [ -e "$PREFIX/bin/pip" ] ; then
+		echo "Can't find pip in virtual environment"
+		exit -1
+	fi
+	PIP=pip
+else
+	echo "Not activating virtual environment, detecting pip"
+	if pip3 --version ; then
+		PIP=pip3
+	elif pip --version ; then
+		PIP=pip
+	else
+		echo "Couldn't detect pip, aborting"
+		exit -1
+	fi
 fi
 
-$PIP install --cache-dir "$D/pipcache" numpy scipy astropy shapely cython matplotlib qpsolvers
+$PIP install --cache-dir "$D/pipcache" setuptools numpy scipy astropy shapely cython matplotlib qpsolvers
 
 if ! $PIP install --cache-dir "$D/pipcache" pycairo ; then
 	echo "WARNING: pycairo could not be installed - perhaps some other package/module"
@@ -160,131 +182,7 @@ for p in ErrUt SerUt EATk GRALE2 ; do
 	make install
 done
 
-BUILDPYQT="no"
-if [ -z "$NOQT" ] ; then
-	PYVER=`python --version 2>&1|cut -f 2 -d " "|cut -f 1 -d .`
-	if [ "$PYVER" = "3" ] ; then
-		X=`which qmake|cat`
-		if ! [ -z "$X" ] ; then
-			X=`qmake --version | grep "Using Qt version 5" | cat`
-			if ! [ -z "$X" ] ; then
-				BUILDPYQT="yes"
-				QTBASE=`which qmake|cat|rev|cut -f 3- -d /|rev`
-				echo "Qt5 detected, will build PyQt5"
-				echo "QTBASE=$QTBASE"
-			else
-				echo "Not building PyQt5, qmake found, but is not for Qt5"
-			fi
-		else
-			echo "Not building PyQt5, qmake not found in PATH"
-		fi
-	else
-		echo "Not building PyQt5, not python 3"
-	fi
-else
-	echo "Environment variable NOQT was set, skipping Qt detection and PyQt configuration"
-fi
-
-function download_and_extract {
-	cd "$PREFIX/src"
-
-	URL="$1"
-	echo "Downloading $URL"
-	FILENAME=`echo "$URL" | rev |cut -f 1 -d / | rev`
-	echo "Filename is $FILENAME"
-	DIRNAME="${FILENAME::${#FILENAME}-7}"
-	echo "Dirname is $DIRNAME"
-
-	if ! [ -e "$DIRNAME" ] ; then
-		if ! [ -e "$FILENAME" ] ; then
-			FOUND="no"
-			for k in {1..10} ; do
-				if curl -L -o "${FILENAME}.tmp" "$URL" ; then
-					FOUND="yes"
-					break
-				else
-					echo "Download failed, retrying in 2 seconds"
-					sleep 2
-				fi
-			done
-
-			if [ "$FOUND" != "yes" ] ; then
-				echo "Unable to download $URL"
-				exit -1
-			fi
-
-			mv "${FILENAME}.tmp" "$FILENAME"
-		else
-			echo "$FILENAME exists, using this" 
-		fi
-
-		tar xfz "$FILENAME"
-	else
-		echo "$DIRNAME exists, using this"
-	fi
-	
-	cd "$DIRNAME"
-}
-
-if [ "$BUILDPYQT" = "yes" ] ; then
-	QTVERSION=`qmake --version |cut -f 4 -d " "`
-	PYQTVERSIONS=`curl https://sourceforge.net/projects/pyqt/files/PyQt5/ |grep "tr title" | grep PyQt-5 | cut -f 2 -d \" |cut -f 2 -d "-"`
-	echo "Qt version detected: $QTVERSION"
-	#echo "PyQt versions available for download: $PYQTVERSIONS" 
-
-	VPYQT=$(python -c "exec(\"import sys\ncompatibleVersion = lambda x, y: '.'.join(x.split('.')[:2]) == '.'.join(y.split('.')[:2])\nqtVersion = sys.argv[1]\nfor pyqtVersion in sys.argv[2:]:\n    if compatibleVersion(qtVersion, pyqtVersion):\n        print(pyqtVersion)\n        sys.exit(0)\nraise Exception('No compatible version found')\n\")" $QTVERSION $PYQTVERSIONS )
-	echo "Compatible PyQt version: $VPYQT"
-	PYQTURL="https://sourceforge.net/projects/pyqt/files/PyQt5/PyQt-${VPYQT}/PyQt5_gpl-${VPYQT}.tar.gz"
-	echo "PyQt URL: $PYQTURL"
-
-	# First download PyQt so that we can check which SIP version to use
-	# (I tried using the latest (4.19.10) with PyQt 5.9.2, but that caused
-	# a segfault due to a sip option '-n' not being specified
-	download_and_extract "$PYQTURL"
-	SIPVERSION=`grep "SIP_MIN_VERSION" configure.py | head -n 1 | cut -f 2 -d "'"`
-	NEEDPYQT5SIP=`grep "from PyQt5 import sip" configure.py |cat`
-	echo "Needed SIP version: $SIPVERSION"
-
-	SIPURL="https://sourceforge.net/projects/pyqt/files/sip/sip-${SIPVERSION}/sip-${SIPVERSION}.tar.gz"
-	echo "SIP URL: $SIPURL"
-
-	# For now we'll always keep this legacy system around
-	# (don't yet know how the new system will need to be used)
-	download_and_extract "$SIPURL"
-	# avoid rebuilding if not necessary
-	if ! [ -e ".buildcomplete" ] ; then
-		rm -f Makefile
-		python configure.py --sysroot="$PREFIX"
-		make clean
-		make install
-
-		if ! [ -z "$NEEDPYQT5SIP" ] ; then
-			rm -f Makefile
-			python configure.py --sysroot="$PREFIX" --sip-module=PyQt5.sip
-			make clean
-			make install
-		fi
-		touch ".buildcomplete"
-	fi
-
-	download_and_extract "$PYQTURL"
-	# avoid rebuilding if not necessary
-	if ! [ -e ".buildcomplete" ] ; then
-		if ! [ -e Makefile ] ; then
-			python configure.py --confirm-license `for i in QtHelp QtMultimedia QtMultimediaWidgets QtNetwork QtPrintSupport QtQml QtQuick QtSql QtSvg QtTest QtWebKit QtWebKitWidgets QtXml QtXmlPatterns QtDesigner QAxContainer QtDBus QtSensors QtSerialPort QtX11Extras QtBluetooth QtMacExtras QtPositioning QtWinExtras QtQuickWidgets QtWebSockets QtWebChannel QtLocation QtNfc QtWebEngineCore QtWebEngine QtWebEngineWidgets Enginio; do echo "--disable $i" ; done`
-			make clean
-		fi
-		# building with multiple cores sometimes appears to fail
-		#make -j $NUMCORES install
-		make install
-		touch ".buildcomplete"
-	fi
-fi
-
 cd "$PREFIX/src/GRALE2/pygrale"
-export SIPINCLUDES="$PREFIX/share/sip/PyQt5/" 
-export QT5INCLUDES="$QTBASE/include:$QTBASE/include/QtCore:$QTBASE/include/QtGui:$QTBASE/include/QtWidgets"
-export QT5LIBDIRS="$QTBASE/lib"
 
 if ! [ -e "Makefile" ] ; then
 	./configure.py
